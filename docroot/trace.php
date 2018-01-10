714da7e20c9da9c2e84815c9ddd342f23bf676e8
salrashid123 <salrashid123@gmail.com>
2018-01-09 21:50:33 -0800

    pin php trace

--- docroot/trace.php
+++ docroot/trace.php
@@ -3,14 +3,26 @@ require 'vendor/autoload.php';
 
 
 use Google\Cloud\Trace\TraceClient;
-
-$client = new TraceClient();
-$trace = $client->trace();
-$span = $trace->span(['name' => 'main']);
-$trace->setSpans([$span]);
-
-$client->insert($trace);
-
+use Google\Cloud\Trace\RequestTracer;
+use Google\Cloud\Trace\Reporter\SyncReporter;
+use Google\Cloud\Trace\Sampler\RandomSampler;
+ 
+$trace = new TraceClient();
+$reporter = new SyncReporter($trace);
+$sampler = new RandomSampler(0.9); 
+RequestTracer::start($reporter, ['sampler' => $sampler]);
+ 
+$p = RequestTracer::inSpan(['name' => 'some-expensive-operation'], function() {
+      sleep(1); 
+      return "done span 1";
+});	
+echo $p."<br/>";
+ 
+$p = RequestTracer::inSpan(['name' => 'another-expensive-operation'], function() {
+      sleep(3); 
+      return "done span2";
+});	
+echo $p."<br/>";
 echo "<br/>";
 
 echo "Trace successful";
