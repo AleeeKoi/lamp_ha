2ceb1bc921599a07c12eab3bacc88e503026351d
salrashid123 <salrashid123@gmail.com>
2018-01-09 21:17:07 -0800

    update trace

--- docroot/trace.php
+++ docroot/trace.php
@@ -3,26 +3,14 @@ require 'vendor/autoload.php';
 
 
 use Google\Cloud\Trace\TraceClient;
-use Google\Cloud\Trace\RequestTracer;
-use Google\Cloud\Trace\Reporter\SyncReporter;
-use Google\Cloud\Trace\Sampler\RandomSampler;
 
-$trace = new TraceClient();
-$reporter = new SyncReporter($trace);
-$sampler = new RandomSampler(0.9); 
-RequestTracer::start($reporter, ['sampler' => $sampler]);
+$client = new TraceClient();
+$trace = $client->trace();
+$span = $trace->span(['name' => 'main']);
+$trace->setSpans([$span]);
 
-$p = RequestTracer::inSpan(['name' => 'some-expensive-operation'], function() {
-     sleep(1); 
-     return "done span 1";
-});	
-echo $p."<br/>";
+$client->insert($trace);
 
-$p = RequestTracer::inSpan(['name' => 'another-expensive-operation'], function() {
-     sleep(3); 
-     return "done span2";
-});	
-echo $p."<br/>";
 echo "<br/>";
 
 echo "Trace successful";
